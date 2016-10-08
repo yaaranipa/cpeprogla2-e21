04864920db13845245df004a7e684978031435e3
Yaara Haina Nipa <nipayaara@gmail.com>
2016-10-08 08:58:12 +0800

    Add files via upload

---
+++ euler3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+	ifstream f("sample.txt");
+	
+	int x, z,total=0;
+	
+	while(f>>z){
+		
+		for (int x=0;x<z;x++){
+			if(x%3==0 || x%5==0){
+			total=total + x;	
+			}
+		}
+		
+	}
+	
+	cout<<total;
+	
+}
+	
+
+	
+	
+
